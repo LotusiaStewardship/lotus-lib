6b934f15cad2f2b581460fc880e0f4bcc8d59206
maff1989 <matthew@urgero.net>
2025-07-26 11:14:36 -0700

    Adjust API types
    Add the new `RankCommentAPI` type for including comments in Profile/Post requests.

--- lib/api.ts
+++ lib/api.ts
@@ -17,6 +17,23 @@ export type PostMeta = {
   txidsUpvoted: string[]
   txidsDownvoted: string[]
 }
+/** Comment data returned from RANK backend API */
+export type RankCommentAPI = {
+  txid: string
+  outIdx: number
+  sats: string
+  firstSeen: string
+  scriptPayload: string
+  instanceId: string
+  height: number
+  data: string
+  feeRate: number
+  platform: string
+  inReplyToProfileId?: string
+  inReplyToPostId?: string
+  repliedProfile?: IndexedProfileRanking
+  repliedPost?: IndexedPostRanking
+}
 /** */
 export type RankAPIParams = {
   platform: string
@@ -29,11 +46,22 @@ export type IndexedProfileRanking = RankAPIParams & {
   satsNegative: string
   votesPositive: number
   votesNegative: number
+  /** Comments associated with the profile */
+  comments?: RankCommentAPI[]
+  /** Posts associated with the profile */
+  posts?: IndexedPostRanking[]
 }
 /** Post ranking returned from RANK backend API */
-export type IndexedPostRanking = IndexedProfileRanking & {
+export type IndexedPostRanking = RankAPIParams & {
+  ranking: string
+  satsPositive: string
+  satsNegative: string
+  votesPositive: number
+  votesNegative: number
   profile: IndexedProfileRanking
   postId: string
+  /** Comments associated with the post */
+  comments?: RankCommentAPI[]
   /** Comment data as a UTF-8 string, if available */
   data?: string
   postMeta?: PostMeta
